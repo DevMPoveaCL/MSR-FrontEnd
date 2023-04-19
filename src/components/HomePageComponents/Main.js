4f895fc0b155437a3473dee22e08e8a2e4128dde
DevMPoveaCL <marcopoveah@gmail.com>
2023-04-19 00:28:43 -0400

    no deploy

--- src/components/HomePageComponents/Main.js
+++ src/components/HomePageComponents/Main.js
@@ -17,13 +17,13 @@ const MainComponent = () => {
               ¿Todo listo para estudiar?
               <a
                 class="sesion btn btn-primary btn-xl text-uppercase mt-5 me-5"
-                href="/Login"
+                href="/MSR-FrontEnd/Login"
               >
                 Inicia sesión
               </a>
               <a
                 class="sesion btn btn-primary "
-                href="/MainView"
+                href="/MSR-FrontEnd/MainView"
               >
                 Continua como invitado
               </a>
