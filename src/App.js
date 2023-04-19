ea3bbaead67c52759145c13310573dbfc0dfe4b5
DevMPoveaCL <marcopoveah@gmail.com>
2023-04-18 23:46:03 -0400

    deploy

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = ()=>{
   return (
     <HashRouter basename="/MSR-FrontEnd">
       <Routes>
-        <Route exact path = "/" element={<HomePage/>}/>
+        <Route exact path = "/MSR-FrontEnd" element={<HomePage/>}/>
         <Route path = "/Login" element={<LoginPage/>}/>
         <Route path = "/Register" element={<RegisterPage/>}/>
         <Route path = "/MainView" element={<MainView/>}/>
